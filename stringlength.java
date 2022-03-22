c58899a417531cfd2ce0ed53a37514ab30426ec0
shambhavani <98842283+shambhavani@users.noreply.github.com>
2022-03-22 15:44:09 +0530

    Add files via upload
    name

---
+++ stringlength.java
@@ -0,0 +1,20 @@
+import java.io*;
+class Stringlength
+{
+public static void main(String args[])
+{
+String str1="Hello";
+String str2=new String("srt1");
+String str3=new String("welcome");
+char ch[]={'A','I','T','S'};
+String strA=new String(ch);
+System.out.println(str1)
+System.out.println(str2)
+System.out.println(str3)
+System.out.println(str4)
+System.out.println(str1.length());
+System.out.println(str2.length());
+System.out.println(str3.length());
+System.out.println(str4.length());
+}
+}
